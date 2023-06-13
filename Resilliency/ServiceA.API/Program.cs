7c7433b2151707f1dbbce096a459ec831c5b865e
ozer.durmaz <ozer.durmaz@hamilton-ch.com>
2023-06-13 13:31:49 +0000

    Resiliency Circuit Breaker

--- Resilliency/ServiceA.API/Program.cs
+++ Resilliency/ServiceA.API/Program.cs
@@ -2,6 +2,10 @@
 using ServiceA.API;
 using Microsoft.Extensions.Http;
 using System;
+using Polly;
+using Polly.Extensions.Http;
+using System.Diagnostics;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,9 +19,41 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient<ProductService>(opt =>
 {
     opt.BaseAddress = new Uri("https://localhost:5003/api/products/");
-}).AddPolicyHandler(GetRetryPolicy());
+}).AddPolicyHandler(GetAdvancedCircuitBreakerPolicy()); //(GetRetryPolicy());
 
- IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+
+IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
+{
+    return HttpPolicyExtensions.HandleTransientHttpError().CircuitBreakerAsync(3, TimeSpan.FromSeconds(30), onBreak: (arg1,arg2) =>
+    {
+        Debug.WriteLine($"Circuit Break => on Break");
+
+    }, onReset: ()=>
+    {
+        Debug.WriteLine($"Circuit Break => on Reset");
+
+    }, onHalfOpen: ()=> {
+        Debug.WriteLine($"Circuit Break => on Half Open");
+    });
+}
+
+
+IAsyncPolicy<HttpResponseMessage> GetAdvancedCircuitBreakerPolicy()
+{
+    return HttpPolicyExtensions.HandleTransientHttpError().AdvancedCircuitBreakerAsync(0.5, TimeSpan.FromSeconds(10), 2, TimeSpan.FromSeconds(100), onBreak: (arg1, arg2) =>
+    {
+        Debug.WriteLine($"Circuit Break => on Break");
+
+    }, onReset: () =>
+    {
+        Debug.WriteLine($"Circuit Break => on Reset");
+
+    }, onHalfOpen: () => {
+        Debug.WriteLine($"Circuit Break => on Half Open");
+    });
+}
+
+IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
 {
     return HttpPolicyExtensions.HandleTransientHttpError().OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound).WaitAndRetryAsync(5, retryAttempt =>
     {
@@ -28,7 +64,7 @@ builder.Services.AddHttpClient<ProductService>(opt =>
 
   Task onRetryAsync(DelegateResult<HttpResponseMessage> arg1, TimeSpan arg2)
 {
-    Debug.WriteLine($"Request is made again: {arg2.TotalMilliSeconds}");
+    Debug.WriteLine($"Request is made again: {arg2.TotalMilliseconds}");
     return Task.CompletedTask;
 }
 
